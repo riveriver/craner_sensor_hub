3a61632901d7539c7155fa2445950045c07b0598
river <hello.river@outlook.com>
2026-07-16 12:48:35 +0800

    feat: 实现动态配置MQTT主题的功能

--- src/mqtt_log_service.c
+++ src/mqtt_log_service.c
@@ -1,10 +1,48 @@
+#include "device_identity_service.h"
 #include "mqtt_service_manager.h"
 
 #include <zephyr/init.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_backend_mqtt.h>
+#include <zephyr/sys/printk.h>
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stddef.h>
 
 LOG_MODULE_REGISTER(mqtt_log_service, CONFIG_LOG_DEFAULT_LEVEL);
+
+static char mqtt_log_topic[96];
+static bool mqtt_log_topic_configured;
+
+static int mqtt_log_service_configure_boot_topic_once(void)
+{
+	int len;
+
+	if (mqtt_log_topic_configured) {
+		return 0;
+	}
+
+	len = snprintk(mqtt_log_topic, sizeof(mqtt_log_topic),
+		       "%s/%s/%s/log/emb",
+		       device_identity_company_get(),
+		       device_identity_project_get(),
+		       device_identity_device_type_get());
+	if (len < 0) {
+		return len;
+	}
+
+	if ((size_t)len >= sizeof(mqtt_log_topic)) {
+		return -EMSGSIZE;
+	}
+
+	len = log_backend_mqtt_topic_set(mqtt_log_topic);
+	if (len == 0) {
+		mqtt_log_topic_configured = true;
+	}
+
+	return len;
+}
 
 static void mqtt_log_service_event_handler(
 	enum mqtt_service_manager_event event, struct mqtt_client *client,
@@ -15,6 +53,7 @@ static void mqtt_log_service_event_handler(
 
 	switch (event) {
 	case MQTT_SERVICE_MANAGER_CONNECTED:
+		(void)mqtt_log_service_configure_boot_topic_once();
 		(void)log_backend_mqtt_client_set(client);
 		break;
 
